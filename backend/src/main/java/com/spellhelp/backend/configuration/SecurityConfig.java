7ad5318375fecb1018f7d41c2c363a8950820cdc
Dan S <dsgenericemail@gmail.com>
2026-01-24 15:50:28 -0800

    feat: Merge backend and frontend auth.

--- backend/src/main/java/com/spellhelp/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/spellhelp/backend/configuration/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -60,5 +61,16 @@ public class SecurityConfig {
         return http.build();
     }
 
-    //  A CorsConfiguration can be added here. Not added because of the Sqlite aspect of the program.
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
---
+++ frontend/src/app/services/auth.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, tap} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+
+  private baseUrl = 'http://localhost:8080/api/auth';
+  private readonly TOKEN_KEY = 'auth_token';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: any): Observable<string> {
+    return this.http.post(`${this.baseUrl}/login`, credentials, {responseType: "text"}).pipe(tap(token => {this.saveToken(token)}));
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  logout():void {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+}
